b0c10a6aeae73c5a450828cdd8b405ac7bea2ded
james717 <www_james_dark@hotmail.com>
2016-05-13 11:54:50 -0400

    Tarea jquery

---
+++ radio.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<script src="assets/js/jquery/jquery-1.12.3.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</head>
+<body>
+<div id="divRadios">
+    <input type="radio" name="color" value="ropero">  ropero
+    <input type="radio" name="color" value="cama">cama
+    <input type="radio" name="color" value="comoda"> comoda
+	<input type="radio" name="color" value="vitrina"> vitrina
+</div>
+<br>
+
+
+
+
+ <input id="iPoner" type="text" name="">
+<button id="bPoner" onclick="eventoCambiar();">Poner</button>
+
+</body>
+</html>
