2da212f7c12f98d20d28dd98ae1e79d4b03e23f3
james717 <www_james_dark@hotmail.com>
2016-05-13 11:55:49 -0400

    Acciones para el archivo radio.php

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 // JavaScript File
+$( document ).ready(function() {
+	var radios = $("#divRadios").find("input:radio");
+	
+	$(radios).change(function(){
+		$("#iPoner").val($(this).val());
+	});
+});
+function eventoCambiar(){
+	alert("Se cambio el select");
+}
