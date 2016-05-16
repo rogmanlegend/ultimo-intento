adfef634065f7f26c9f79b68c504a4870ec2157b
james717 <www_james_dark@hotmail.com>
2016-05-16 13:13:02 -0400

    formulariocrearpersonajames

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,30 @@ $( document ).ready(function() {
 	$(radios).change(function(){
 		$("#iPoner").val($(this).val());
 	});
+	
+	$("#f_crear_per").submit(function(event){	
+		//alert("sssss");
+		$("#s_nombres").empty();
+		$("#s_nombres").append($("#nombre").val());
+		$("#s_apellidom").append($("#apellidoP").val());
+		$("#s_apellidop").append($("#apellidoM").val());
+		$("#s_email").append($("#email").val());
+		$("#s_telcel").append($("#teltel").val());
+		
+		event.preventDefault();
+	});
+$("#f_crear_per").submit(function(event){	
+		//alert("sssss");
+		$("#s_nombres").empty();
+		$("#s_nombres").append($("#nombre").val());
+		$("#s_apellidom").append($("#apellidoP").val());
+		$("#s_apellidop").append($("#apellidoM").val());
+		$("#s_email").append($("#email").val());
+		$("#s_telcel").append($("#teltel").val());
+		
+		event.preventDefault();
+	});
+
 });
 function eventoCambiar(){
 	alert("Se cambio el select");
